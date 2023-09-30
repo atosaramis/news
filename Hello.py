caf12c9d3397422e2521cdd8399bc009dbc3e1c3
atosaramis <78604182+atosaramis@users.noreply.github.com>
2023-09-30 16:44:11 -0400

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -12,42 +12,47 @@ def fetch_news(api_key, query, from_date, to_date, num_results):
         "pageSize": num_results
     }
     response = requests.get(url, params=params)
+    data = response.json()
+    st.write("API Response:", data)  # Display the response data on the app
+    
     if response.status_code != 200:
         st.error(f"Failed to retrieve news: {response.text}")
         return []
     else:
-        return response.json()['articles']
+        return data['articles']
 
 def main():
     st.title("News API Searcher")
     
-    # Initialize session state variables
-    if 'api_key' not in st.session_state:
-        st.session_state.api_key = ""
-    if 'query' not in st.session_state:
-        st.session_state.query = "Python"
-    if 'date_range' not in st.session_state:
-        st.session_state.date_range = [datetime.now().date(), datetime.now().date()]
-    if 'num_results' not in st.session_state:
-        st.session_state.num_results = 10
+    api_key = st.text_input("Enter your NewsAPI key:", type="password")
+    st.write("API Key:", api_key)  # Display the entered API key
     
-    st.session_state.api_key = st.text_input("Enter your NewsAPI key:", value=st.session_state.api_key, type="password")
-    st.session_state.query = st.text_input("Search Term:", value=st.session_state.query)
-    st.session_state.date_range = st.date_input("Date Range:", value=st.session_state.date_range)
-    st.session_state.num_results = st.number_input("Number of Results to Return:", 1, 100, value=st.session_state.num_results)
-    
-    if st.button("Fetch News"):
-        from_date = st.session_state.date_range[0].strftime('%Y-%m-%d')
-        to_date = st.session_state.date_range[1].strftime('%Y-%m-%d')
+    if api_key:
+        query = st.text_input("Search Term:", "Python")
+        date_range = st.date_input("Date Range:", [datetime.now().date(), datetime.now().date()])
+        num_results = st.number_input("Number of Results to Return:", 1, 100, 10)
         
-        articles = fetch_news(st.session_state.api_key, st.session_state.query, from_date, to_date, st.session_state.num_results)
+        st.write("Query:", query)  # Display the entered query
+        st.write("Date Range:", date_range)  # Display the selected date range
+        st.write("Number of Results:", num_results)  # Display the number of results
         
-        for article in articles:
-            st.write(f"### [{article['title']}]({article['url']})")
-            st.write(f"- Source: {article['source']['name']}")
-            st.write(f"- Published At: {article['publishedAt']}")
-            st.write(f"- Description: {article['description']}")
-            st.write("---")
+        if st.button("Fetch News"):
+            from_date = date_range[0].strftime('%Y-%m-%d')
+            to_date = date_range[1].strftime('%Y-%m-%d')
+            
+            st.write("Fetching news for:", query, "from", from_date, "to", to_date)  # Display the fetch details
+            
+            articles = fetch_news(api_key, query, from_date, to_date, num_results)
+            
+            if not articles:
+                st.write("No articles found.")  # Display a message if no articles are found
+            
+            for article in articles:
+                st.write(f"### [{article['title']}]({article['url']})")
+                st.write(f"- Source: {article['source']['name']}")
+                st.write(f"- Published At: {article['publishedAt']}")
+                st.write(f"- Description: {article['description']}")
+                st.write("---")
 
 if __name__ == "__main__":
     main()
