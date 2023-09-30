6ce391a7a731b1ebb6879ec05a9c7dfc639ec6d0
atosaramis <78604182+atosaramis@users.noreply.github.com>
2023-09-30 15:26:25 -0400

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,34 +1,45 @@
 import streamlit as st
 import requests
+import json
 from datetime import datetime
-import pandas as pd
-import base64
 
-# Function to get the news articles
-def get_articles(api_key, keyword, from_date, to_date, num_results):
+def fetch_news(api_key, query, from_date, to_date, num_results):
     url = "https://newsapi.org/v2/everything"
     params = {
-        "q": keyword,
+        "q": query,
         "from": from_date,
         "to": to_date,
-        "pageSize": num_results,
-        "apiKey": api_key
+        "apiKey": api_key,
+        "pageSize": num_results
     }
     response = requests.get(url, params=params)
-    return response.json()
+    if response.status_code != 200:
+        st.error(f"Failed to retrieve news: {response.text}")
+    else:
+        return response.json()['articles']
 
-# Streamlit app
-st.title('News Search')
+def main():
+    st.title("News API Searcher")
+    
+    api_key = st.text_input("Enter your NewsAPI key:", type="password")
+    
+    if api_key:
+        query = st.text_input("Search Term:", "Python")
+        date_range = st.date_input("Date Range:", [datetime.now().date(), datetime.now().date()])
+        num_results = st.number_input("Number of Results to Return:", 1, 100, 10)
+        
+        if st.button("Fetch News"):
+            from_date = date_range[0].strftime('%Y-%m-%d')
+            to_date = date_range[1].strftime('%Y-%m-%d')
+            
+            articles = fetch_news(api_key, query, from_date, to_date, num_results)
+            
+            for article in articles:
+                st.write(f"### [{article['title']}]({article['url']})")
+                st.write(f"- Source: {article['source']['name']}")
+                st.write(f"- Published At: {article['publishedAt']}")
+                st.write(f"- Description: {article['description']}")
+                st.write("---")
 
-api_key = st.text_input('Enter your NewsAPI API key', type="password")  # Hide the API key
-
-if api_key:
-    keyword = st.text_input('Enter a keyword to search for')
-    from_date = st.date_input('Choose a start date')
-    to_date = st.date_input('Choose an end date')
-    num_results = st.number_input('Enter the number of results to return', min_value=1, max_value=100)
-
-    if keyword and from_date and to_date and num_results:
-        articles = get_articles(api_key, keyword, from_date.strftime('%Y-%m-%d'), to_date.strftime('%Y-%m-%d'), num_results)
-        df = pd.DataFrame(articles['articles'])
-        st.markdown(df.to_markdown())
+if __name__ == "__main__":
+    main()
