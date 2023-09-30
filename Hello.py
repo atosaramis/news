0b2a1b3fa95bcf596a8b0c7e8fbf2ec618fc794e
atosaramis <78604182+atosaramis@users.noreply.github.com>
2023-09-30 16:28:17 -0400

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,12 +1,11 @@
 import streamlit as st
-from datetime import datetime
 import requests
-from Bio import Entrez
+from datetime import datetime
 
 # Requirements.txt file:
 # streamlit
 # requests
-# biopython
+
 
 def fetch_news(api_key, query, from_date, to_date, num_results):
   """Fetches news articles from the NewsAPI.
@@ -27,50 +26,16 @@ def fetch_news(api_key, query, from_date, to_date, num_results):
   articles = response.json()['articles']
   return articles
 
-def fetch_pubmed(query, from_date, to_date, num_results, email):
-  """Fetches PubMed articles.
 
-  Args:
-    query: The search query.
-    from_date: The start date for the search.
-    to_date: The end date for the search.
-    num_results: The number of results to return.
-    email: The user's email address.
+menu = st.sidebar.selectbox("Choose a section", ["News API Searcher"])
 
-  Returns:
-    A list of PubMed articles.
-  """
-
-  Entrez.email = email
-  handler = Entrez.esearch(db="pubmed", term=query, datetype="pdat", mindate=from_date, maxdate=to_date, retmax=num_results)
-  record = Entrez.read(handler)
-  pmids = record["IdList"]
-
-  articles = []
-  for pmid in pmids:
-    article = Entrez.efetch(db="pubmed", id=pmid, retmode="xml")
-    article_record = Entrez.read(article)[0]
-
-    articles.append({
-      "title": article_record["Article"]["ArticleTitle"],
-      "abstract": article_record["Article"]["Abstract"]["AbstractText"][0],
-      "url": f"https://pubmed.ncbi.nlm.nih.gov/{pmid}",
-      "authors": [author["LastName"] + ", " + author["FirstName"] for author in article_record["Article"]["AuthorList"]],
-      "journal": article_record["Article"]["Journal"],
-      "publication_date": article_record["Article"]["PublicationDate"],
-    })
-
-  return articles
-
-menu = st.sidebar.selectbox("Choose a section", ["PubMed Searcher", "News API Searcher"])
-
-if menu == "PubMed Searcher":
-  st.title("PubMed Searcher")
+if menu == "News API Searcher":
+  st.title("News API Searcher")
 
   # Validate the user input.
-  email = st.text_input("Enter your email:")
-  if not email:
-    st.error("Please enter a valid email address.")
+  api_key = st.text_input("Enter your NewsAPI key:")
+  if not api_key:
+    st.error("Please enter a valid NewsAPI key.")
     st.stop()
 
   # Display the search form.
@@ -82,13 +47,12 @@ if menu == "PubMed Searcher":
   # Fetch the search results.
   if st.button("Fetch Articles"):
     st.write("Fetching articles...")
-    articles = fetch_pubmed(query, from_date, to_date, num_results, email)
+    articles = fetch_news(api_key, query, from_date, to_date, num_results)
 
     # Display the search results.
     if articles:
       st.write("Search results:")
       for article in articles:
         st.write(f"### [{article['title']}]({article['url']})")
-        st.write(f"- Authors: {', '.join(article['authors'])}")
-        st.write(f"- Journal: {article['journal']}")
-        st.write(f"- Publication Date: {article['publication_date']}")
+        st.write(f"- Source: {article['source']['name']}")
+        st.write(f"- Published At: {article['publishedAt']}")
